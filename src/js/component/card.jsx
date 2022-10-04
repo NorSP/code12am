5e57d66fbc2a51a2900020898935b86d11e66535
NorSP <norbissilvera@gmail.com>
2022-10-04 14:42:52 +0000

    comentario

---
+++ src/js/component/card.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Card = () => {
+	return (
+		<div>
+            <div className="row row-cols-1 row-cols-md-3 g-4">
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://i.picsum.photos/id/395/350/200.jpg?hmac=Jimc_watdzVfHvl3VERjvMHnbQB0fWI0BmYqKKCBEJs" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://i.picsum.photos/id/395/350/200.jpg?hmac=Jimc_watdzVfHvl3VERjvMHnbQB0fWI0BmYqKKCBEJs" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a short card.</p>
+      </div>
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://i.picsum.photos/id/395/350/200.jpg?hmac=Jimc_watdzVfHvl3VERjvMHnbQB0fWI0BmYqKKCBEJs" className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  
+</div>
+
+</div>
+	);
+};
+export default Card;
